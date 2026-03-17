26d7df22a4bac2c996817847866db9590373f97c
ps9102-hub <ps9102@srmist.edu.in>
2026-03-17 08:55:57 +0530

    Coded UC5: Display Hello with multiple args using enhanced for loop or default

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,24 +1,17 @@
 public class HelloApp {
     public static void main(String[] args) {
-        // Check if any command-line arguments are provided
+        
         if (args.length == 0) {
             System.out.println("Hello, World!");
             return;
         }
-
-        // Build greeting for multiple names
         String greeting = "Hello, ";
         for (String name : args) {
             greeting += name + ", ";
         }
-
-        // Remove the trailing comma and space
         greeting = greeting.substring(0, greeting.length() - 2);
-
-        // Add exclamation mark
         greeting += "!";
 
-        // Print the final greeting
         System.out.println(greeting);
     }
 }
